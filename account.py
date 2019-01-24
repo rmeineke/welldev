ddf71915179515481dc44de97308540178d129fc
Robert Meineke <rmeineke@gmail.com>
2019-01-24 09:47:51 -0800

    some new getters/setters in the pythonic way

--- account.py
+++ account.py
@@ -9,8 +9,8 @@ class Account:
         self.addr = addr
         self.reads_in = reads_in
         self.master = master
-        self.previous_reading = -999
-        self.latest_reading = -999
+        self.__previous_reading = -999
+        self.__latest_reading = -999
         self.current_usage = -999
         self.current_usage_percent = -999
         self.ttl_usage = -999
@@ -18,12 +18,29 @@ class Account:
         self.pge_bill_share = -999
         self.savings_assessment = -999
 
+    #
+    # def __setattr__(self, name, value):
+    #     super().__setattr__(name, value)
+
+    @property
+    def latest_reading(self):
+        return self.__latest_reading
+
+    @latest_reading.setter
+    def latest_reading(self, value):
+        self.__latest_reading = value
+
+    @property
+    def previous_reading(self):
+        return self.__previous_reading
+
+    @previous_reading.setter
+    def previous_reading(self, value):
+        self.__previous_reading = value
+
     def calculate_current_usage(self):
         self.current_usage = self.latest_reading - self.previous_reading
         if self.reads_in == 'cubic feet':
             self.current_usage *= 7.48052
             self.current_usage = round(self.current_usage, 2)
         self.current_usage = round(self.current_usage, 2)
-
-    def __setattr__(self, name, value):
-        super().__setattr__(name, value)
