160b596262c67bf068cda6d12f29d18d1780bd1f
Robert Meineke <rmeineke@gmail.com>
2019-02-07 03:14:51 -0800

    code cleanup

--- lib/utils.py
+++ lib/utils.py
@@ -5,57 +5,50 @@ from shutil import copyfile
 
 
 def backup_file(logger, fn):
-    logger.debug('entering backup_file')
-    backup_directory = './backups'
+    logger.debug("entering backup_file")
+    backup_directory = "./backups"
     if not os.path.exists(backup_directory):
         os.makedirs(backup_directory)
 
-    dt = datetime.datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
+    dt = datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
 
-    new_filename = os.path.join(backup_directory, dt + '__' + fn)
-    logger.debug(f'backing up to: {new_filename}')
+    new_filename = os.path.join(backup_directory, dt + "__" + fn)
+    logger.debug(f"backing up to: {new_filename}")
     copyfile(fn, new_filename)
 
 
 def prompt_for_amount(logger, prompt):
-    logger.debug('entering get_amount()')
+    logger.debug("entering get_amount()")
     while 1:
         try:
             amt = input(f"{prompt}: $")
-
-            # amt = round(float(amt) * 100, 2)
-            # print(f'{amt}')
-            #
-
-
-            logger.debug(f'get_amount() input is: {amt}')
-            # "{:.2f}".format(float(amt))
+            logger.debug(f"get_amount() input is: {amt}")
             amt = int(float(amt) * 100)
-            logger.debug(f'get_amount() is going to return: {amt}')
+            logger.debug(f"get_amount() is going to return: {amt}")
             return amt
         except ValueError as e:
             logger.debug(e)
-            print('Bad amount ... try again.')
+            print("Bad amount ... try again.")
 
 
 def prompt_for_current_date(logger, prompt):
-    logger.debug('entering prompt_for_current_date')
+    logger.debug("entering prompt_for_current_date")
     while 1:
         try:
             reading_date = input("{}: ".format(prompt))
-            date_obj = datetime.datetime.strptime(reading_date, '%m/%d/%Y')
-            return datetime.datetime.strftime(date_obj, '%Y-%m-%d')
+            date_obj = datetime.datetime.strptime(reading_date, "%m/%d/%Y")
+            return datetime.datetime.strftime(date_obj, "%Y-%m-%d")
         except ValueError:
-            print('Bad date ... try again.')
+            print("Bad date ... try again.")
 
 
 def prompt_for_notes(logger, prompt):
-    logger.debug('entering prompt_for_notes')
+    logger.debug("entering prompt_for_notes")
 
-    input_str = ''
+    input_str = ""
     while 1:
         response = input("{} [q to quit]: ".format(prompt))
-        if response == 'q':
+        if response == "q":
             break
         input_str += response
     return input_str.strip()
@@ -63,29 +56,29 @@ def prompt_for_notes(logger, prompt):
 
 def prompt_for_account(logger, prompt, cur):
     # get the account list
-    cur.execute('SELECT * FROM accounts')
+    cur.execute("SELECT * FROM accounts")
     rows = cur.fetchall()
     acct_list = []
     for r in rows:
-        print('{}: {}'.format(r[0], r[2]))
+        print("{}: {}".format(r[0], r[2]))
         acct_list.append(r[0])
 
     while True:
-        acct = input('{}: '.format(prompt))
+        acct = input("{}: ".format(prompt))
         if int(acct) in acct_list:
             return acct
 
+#
+# def get_savings_balance(logger, db):
+#     # this is way neater than what follows
+#     logger.debug("Entering get_savings_balance()")
+#     row = cur.execute("SELECT sum(amount) FROM savings_account")
+#     cur_balance = row.fetchone()[0]
+#     return cur_balance
+
 
 def get_savings_balance(logger, db):
-    # this is way neater than what follows
-    logger.debug('Entering get_savings_balance()')
-    row = cur.execute("SELECT sum(amount) FROM savings_account")
-    cur_balance = row.fetchone()[0]
-    return cur_balance
-
-
-def get_savings_balance(logger, db):
-    logger.debug('Entering get_savings_balance()')
+    logger.debug("Entering get_savings_balance()")
     savings_balance = 0
 
     db = sqlite3.connect(db)
@@ -96,7 +89,7 @@ def get_savings_balance(logger, db):
     logger.debug(rows)
 
     for row in rows:
-        savings_balance += row['amount']
+        savings_balance += row["amount"]
 
     # save, then close the cursor and db
     db.commit()
@@ -107,9 +100,14 @@ def get_savings_balance(logger, db):
 
 
 def get_balance_from_transaction_log(db, logger):
+    logger.debug("Entering get_balance_from_transaction_log")
     db = sqlite3.connect(db)
     cur = db.cursor()
-    row = cur.execute("SELECT SUM(transaction_amount) FROM transactions_log WHERE transaction_type = 4 OR transaction_type = 5 OR transaction_type = 6  OR transaction_type = 7")
+    row = cur.execute(
+        "SELECT SUM(transaction_amount) "
+        "FROM transactions_log "
+        "WHERE transaction_type = 4 OR transaction_type = 5 OR transaction_type = 6  OR transaction_type = 7"
+    )
     cur_balance = row.fetchone()[0]
     cur.close()
     db.close()
