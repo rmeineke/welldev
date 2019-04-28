baa04aa384f93f04aae7476c8ab0c71060206568
Robert Meineke <rmeineke@gmail.com>
2019-04-28 00:53:00 -0700

    account adjustment was negating the value

--- account_adjustment.py
+++ account_adjustment.py
@@ -27,16 +27,14 @@ def main():
     db.row_factory = sqlite3.Row
     cur = db.cursor()
 
-    const = constants.Constants()
-    print(f'{const.account_adjustment}')
-
     logger.debug('attempting to backup the database file now')
     utils.backup_file(logger, database)
 
+    const = constants.Constants()
     account = utils.prompt_for_account(logger, 'Choose the affected account', cur)
     adjustment_date = utils.prompt_for_current_date(logger, 'Adjustment date')
     adjustment_amount = utils.prompt_for_amount(logger, 'Adjustment amount')
-    adjustment_amount *= -1
+    # adjustment_amount *= -1
     adjustment_note = utils.prompt_for_notes(logger, 'Adjustment note')
     logger.debug(f'account: {account}')
     adjustment_date = f"{adjustment_date}"
@@ -61,25 +59,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-#
-#
-# 1
-#
-# The problem here is understanding parameterization - it works only for parameters, not for column names and other stuff.
-#
-# in this example:
-#
-#  query = 'SELECT * FROM foo WHERE bar = ? AND baz = ?'
-#  params = (a, b)
-#  cursor.execute(query, params)
-#
-# Note that the query and the data are passed separately to .execute - it is the database's job to do the interpolation - that frees you from quote hell, and makes your program safer by disabling any kind of sql injection. It also could perform better - it allows the database to cache the compiled query and use it when you change parameters.
-#
-# Now that only works for data. If you want to have the actual column name in a variable, you have to interpolate it yourself in the query:
-#
-#  col1 = 'bar'
-#  col2 = 'baz'
-#  query = f'SELECT * FROM foo WHERE {col1} = ? AND {col2} = ?'
-#  cursor.execute(query, params)
-#
-# shareeditflag
