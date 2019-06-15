61fbb6376ee048d3e6be34e78abaa877b1dc136c
Robert Meineke <rmeineke@gmail.com>
2019-06-15 00:00:05 -0700

    refactoring for the new logger

--- pge_bill_paid.py
+++ pge_bill_paid.py
@@ -1,61 +1,22 @@
-import logging
-import sys
 import sqlite3
 from lib import utils
 from lib import constants
+import logbook
 
 
 def main():
     database = 'well.sqlite'
+    logger = logbook.Logger('pge_bill_paid')
 
-    # set up for logging
-    LEVELS = {'debug': logging.DEBUG,
-              'info': logging.INFO,
-              'warning': logging.WARNING,
-              'error': logging.ERROR,
-              'critical': logging.CRITICAL,
-              }
-    if len(sys.argv) > 1:
-        level_name = sys.argv[1]
-        level = LEVELS.get(level_name, logging.NOTSET)
-        logging.basicConfig(level=level)
-
-    logger = logging.getLogger()
-    logger.debug('Entering main')
-    utils.backup_file(logger, database)
+    utils.backup_file(database)
     db = sqlite3.connect(database)
     db.row_factory = sqlite3.Row
     cur = db.cursor()
 
     # get date_paid
-    payment_date = utils.prompt_for_current_date(logger, "Date paid")
+    payment_date = utils.prompt_for_current_date("Date paid")
     # get amount
-    payment_amount = utils.prompt_for_amount(logger, "Amount paid")
-
-    # # mark the main account as paid
-    # cur.execute('SELECT acct_id FROM account WHERE master = "yes"')
-    #
-    # # fetchone ROW .... zeroeth item is the account number
-    # master_acct_id = cur.fetchone()[0]
-    # logger.debug(f'master_acct_id: {master_acct_id}')
-    #
-    # # go to the master account
-    # # find the last billing amount
-    # # 2019.02.19 ... had to tweak this as it was grabbing the wrong amount due to a sorting error
-    # exec_str = f"""
-    #     SELECT amount
-    #     FROM activity
-    #     WHERE acct = (?)
-    #     AND note
-    #     LIKE "%PGE bill share%"
-    #     ORDER BY date
-    #     DESC LIMIT 1
-    # """
-    # params = (master_acct_id, )
-    # cur.execute(exec_str, params)
-    # last_billing_amt = cur.fetchone()[0]
-    # logger.debug(f'last_billing_amt: {last_billing_amt}')
-
+    payment_amount = utils.prompt_for_amount("Amount paid")
     const = constants.Constants()
 
     # mark the bill paid in the activity account
@@ -65,16 +26,8 @@ def main():
                 VALUES (?, ?, ?, ?)
             """
     params = (payment_date, const.pge_bill_paid, payment_amount, "PGE bill paid")
+    logger.trace(params)
     cur.execute(exec_str, params)
-
-    # # credit the master account with the equivalent payment
-    # last_billing_amt = last_billing_amt * -1
-    # exec_str = f"""
-    #     INSERT INTO activity (date, acct, type, amount, note)
-    #     VALUES (?, ?, ?, ?, ?)
-    # """
-    # params = (payment_date, master_acct_id, const.payment, last_billing_amt, "Payment on account (PGE bill paid)")
-    # cur.execute(exec_str, params)
 
     # save, then close the cursor and db
     db.commit()
@@ -83,4 +36,5 @@ def main():
 
 
 if __name__ == '__main__':
+    utils.init_logging('pge_bill_paid.log')
     main()
